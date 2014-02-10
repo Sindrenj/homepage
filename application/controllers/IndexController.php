587023de179b3ed9508dd92ded164e89b7b53860
Sindrenj <sindre@sindrenj.net>
2014-02-10 23:55:11 +0100

    Gjorde noen småendringer i controllere og html-filer. Slettet viewAll.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -19,12 +19,12 @@ class IndexController extends \Phalcon\Mvc\Controller {
     }
   
     public function aboutAction() {
-        echo "Test of session.";
-        if( isset( $this->session->get('auth')['username'] ) ) {
-            echo "Hello, this is the secret message! The answer to life the universe and everything.: 42.";
-        } else {
-            echo "You have no access here.";
-        }
+        echo "This is the about-page!";
+//        if( isset( $this->session->get('auth')['username'] ) ) {
+//            echo "Hello, this is the secret message! The answer to life the universe and everything.: 42.";
+//        } else {
+//            echo "You have no access here.";
+//        }
         
         //Remember to set the page-title:
         $this->view->setVar('pageTitle', 'About');
--- application/controllers/ProjectsController.php
+++ application/controllers/ProjectsController.php
@@ -4,7 +4,7 @@ class ProjectsController extends \Phalcon\Mvc\Controller {
    
     public function indexAction() {
         //Assign the projects to the view:
-        $this->view->setVar('project', Project::find());
+        $this->view->setVar('projects', Project::find());
     }
     
     public function viewAction( $id ) {}
